2726c8a8e2b2d6f1d86bfd7e383e1640da41af33
the-chicken-leg <michael.koch3@pm.me>
2026-03-04 12:18:33 +0100

    can upload thumbnail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -15,23 +16,54 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
 		return
 	}
+	dbVideo, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
 
 	token, err := auth.GetBearerToken(r.Header)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
 		return
 	}
-
 	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
 		return
 	}
+	if userID != dbVideo.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Video does not belong to user", err)
+		return
+	}
 
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+	
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Could not read thumbnail", err)
+		return
+	}
+	mediaType := header.Header.Get("Content-Type")
+	videoThumbnails[dbVideo.ID] = thumbnail{
+		data: data,
+		mediaType: mediaType,
+	}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, dbVideo.ID)
+	dbVideo.ThumbnailURL = &thumbnailURL
+	cfg.db.UpdateVideo(dbVideo)
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	respondWithJSON(w, http.StatusOK, dbVideo)
 }
---
+++ scripts/connect_db.sh
@@ -0,0 +1,3 @@
+cd ~/workspace/github.com/the-chicken-leg/learn-file-storage-s3-golang-starter
+sqlite3 tubely.db -header -column
+
